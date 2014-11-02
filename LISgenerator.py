efd784b57da4eafe92020315667c27bc204dc1cb
Priyanka Menghani <priyanka.menghani@gmail.com>
2014-11-02 14:24:41 -0600

    fixed some bugs

--- LISgenerator.py
+++ LISgenerator.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 
 #TODO: create input files and fill the arrays from that
-numbers = [2, 8, 3, 5, 7, 11]
-weights = [1, 10, 4, 1, 4, 5 ]
+numbers = [3, -9, 8, 9, 6, -2, 9, 1, -2, 6, 7, 3, 3, -1, 1, -3, 0, 3, 1,-3]
+weights = [5, 9, 1, 10, 8, 5, 3, 10, 6, 5, 4, 2, 5, 8, 4, 3, 9, 8, 4, 7]
 LIS = []
 maxWeight = []
 k = []
@@ -26,16 +26,19 @@ for i in range(len(numbers)):
 
 for i in range(1, len(numbers)):
 	for j in range(0, i):
-		if numbers[j] < numbers[i] and LIS[i] < LIS[j] + 1 and maxWeight[i] < maxWeight[j] + weights[i]:
+		if numbers[j] < numbers[i] and maxWeight[i] < maxWeight[j] + weights[i]:
 			maxWeight[i] = maxWeight[j] + weights[i]
+			print(" adding " , maxWeight[j] , " and " , weights[i] , " for " , numbers[i] , " and " , numbers[j])
 			LIS[i] = LIS[j] + 1
+			k[i] = k[j]
 			pred[i] = j
-		elif numbers[j] > numbers[i] and LIS[i] < LIS[j] + 1 and k[j] + 1 <= maxK and maxWeight[i] < maxWeight[j] + weights[i] - weights[i]*(numbers[j] - numbers[i]):
+		elif numbers[j] >= numbers[i] and k[j] + 1 <= maxK and maxWeight[i] < maxWeight[j] + weights[i] - weights[i]*(numbers[j] - numbers[i]):
 			maxWeight[i] = maxWeight[j] + weights[i] - weights[i]*(numbers[j] - numbers[i])
 			k[i] = k[j] + 1
 			LIS[i] = LIS[j] + 1
 			pred[i] = j
 
+print(maxWeight)
 print("Solution for part b:")
 # Find the maximum weight from the maxWeight matrix
 maxWeightFound = max(maxWeight)
@@ -53,5 +56,5 @@ print(sequenceIndices)
 sequence = []
 for i in range(len(sequenceIndices)):
 	sequence.append(numbers[sequenceIndices[i]])
-	
+
 print(sequence)
